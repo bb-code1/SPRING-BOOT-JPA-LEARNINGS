a6f4e8e154ec7c77cda96599f6eed61b7c5f29c5
bb-code1 <barkatbashir10@gmail.com>
2026-02-03 18:00:00 +0530

    Bootstrap Spring Boot 3 Starter project skeleton

---
+++ src/test/java/com/zbs/learning/SpringDataJpaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbs.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
